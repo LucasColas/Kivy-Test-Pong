cc2b25c46f32aa292a4d6e7a17277c6ba7cac5b7
LucasColas <lucas.colas13@gmail.com>
2022-09-10 10:11:00 +0200

    Menu

--- main.py
+++ main.py
@@ -3,7 +3,30 @@ from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
+from kivy.core.window import Window
+from kivy.uix.screenmanager import ScreenManager, Screen
+
 from random import randint
+
+
+"""
+TODO :
+screen Manager : https://kivy.org/doc/stable/api-kivy.uix.screenmanager.html
+
+"""
+
+class PongPaddle(Widget):
+    score = NumericProperty(0)
+
+    def bounce_ball(self, ball):
+        if self.collide_widget(ball):
+            vx, vy = ball.velocity
+            offset = (ball.center_y - self.center_y) / (self.height / 2)
+            bounced = Vector(-1 * vx, vy)
+            vel = bounced * 1.1
+            ball.velocity = vel.x, vel.y + offset
+
+
 
 class PongBall(Widget):
     velocity_x = NumericProperty(0)
@@ -19,28 +42,80 @@ class PongBall(Widget):
 class PongGame(Widget):
     ball = ObjectProperty(None)
 
-    def serve_ball(self):
+    player1 = ObjectProperty(None)
+    player2 = ObjectProperty(None)
+
+    def __init__(self, **kwargs):
+        super(PongGame, self).__init__(**kwargs)
+        self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
+        self._keyboard.bind(on_key_down=self._on_keyboard_down)
+
+    def _keyboard_closed(self):
+        self._keyboard.unbind(on_key_down=self._on_keyboard_down)
+        self._keyboard = None
+
+    def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
+        if keycode[1] == 'w':
+            self.player1.center_y += 10
+        elif keycode[1] == 's':
+            self.player1.center_y -= 10
+        elif keycode[1] == 'up':
+            self.player2.center_y += 10
+        elif keycode[1] == 'down':
+            self.player2.center_y -= 10
+        return True
+
+    def serve_ball(self, vel=(4, 0)):
         self.ball.center = self.center
-        self.ball.velocity = Vector(4,0).rotate(randint(0,360))
+        self.ball.velocity = vel
 
     def update(self, dt):
         self.ball.move()
 
+        self.player1.bounce_ball(self.ball)
+        self.player2.bounce_ball(self.ball)
+
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velocity_y *= -1
 
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.velocity_x *= -1
+        if self.ball.x < self.x:
+            self.player2.score += 1
+            self.serve_ball(vel=(4, 0))
+        if self.ball.right > self.width:
+            self.player1.score += 1
+            self.serve_ball(vel=(-4, 0))
+
+        def on_touch_move(self, touch):
+            if touch.x < self.width / 3:
+                self.player1.center_y = touch.y
+            if touch.x > self.width - self.width / 3:
+                self.player2.center_y = touch.y
+
+class MenuScreen(Screen):
+    pass
+
+class GameScreen(Screen):
+    def __init__(self, **kwargs):
+        super(GameScreen, self).__init__(**kwargs)
+        self.game = PongGame()
+        self.add_widget(self.game)
+        #self.game.serve_ball()
+        Clock.schedule_interval(self.game.update, 1.0/60.0)
+
+
 
 class PongApp(App):
     def build(self):
+        """
         game = PongGame()
         game.serve_ball()
         Clock.schedule_interval(game.update, 1.0/60.0)
         return game
-
-
-
+        """
+        sm = ScreenManager()
+        sm.add_widget(MenuScreen(name='menu'))
+        sm.add_widget(GameScreen(name='PongGame'))
+        return sm
 
 if __name__ == '__main__':
     PongApp().run()
